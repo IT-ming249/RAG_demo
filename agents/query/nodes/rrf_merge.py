8e02958a1fac0d34059b3b9b44dfbf655a8ba2a0
ming <1045109874@qq.com>
2026-09-24 16:59:07 +0800

    9.24

--- agents/query/nodes/rrf_merge.py
+++ agents/query/nodes/rrf_merge.py
@@ -10,4 +10,14 @@ async def rrf_merge(state: QueryGraphState, runtime: Runtime[QueryGraphContext])
     hyde_chunks = state.hyde_chunks
 
     assert embedding_chunks is not None
-    assert  hyde_chunks is not None
+    assert hyde_chunks is not None
+
+    try:
+        # 1. 两路向量搜索结果按照得分排序
+        embedding_chunks.sort(key=lambda chunk: chunk.distance, reverse=True)
+        hyde_chunks.sort(key=lambda chunk: chunk.distance, reverse=True)
+
+
+    except Exception as e:
+        writer(QueryGraphStepInfo(name="RRF排序", status="failed", error=str(e)))
+        return {"should_continue": False}
